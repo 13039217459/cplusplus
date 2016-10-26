9e41a88ea04fb3da38f50ad5a5e5da1cceba8b4f
罗哲夫 <974806047@qq.com>
2016-10-26 18:16:57 +0800

    矩阵的一些运算

---
+++ 图论/图论/数组.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+
+#ifndef SHUZHU_CPP
+#define SHUZHU_CPP
+
+class shuzhu{
+public:
+	int     **a;
+	int     x, y;
+	shuzhu(int x1,int y1){
+		x = x1;
+		y = y1;
+		a = (int **)malloc(sizeof(int *)*x);
+
+		for (int i = 0; i < x; i++)
+		{
+			*(a + i) = (int  *)malloc(sizeof(int)*y);
+		}
+		for (int i = 0; i < x; i++)
+		{
+			for (int j = 0; j < y; j++){
+				a[i][j] = 0;
+			}
+
+		}
+	}
+	template<int Fuck>
+	int  set(int (&p)[Fuck]){                                    //想要获取传进来的数组的长度只有这种方法
+																//当这个数组名作为参数传递给print这个函数的时候，函数中得到的是一个int的指针。
+																//而数组名其实是一个常量指针。sizeof对待这两者是不一样的。
+		int len = sizeof(p) / sizeof(p[0]);
+		/*cout << "sizeof(p):" << sizeof(p) << endl;
+		cout << "sizeof(p[0])" << sizeof(p[0]) << endl;
+		cout << "  len2："<<len << endl;*/
+		int ii = 0;
+		if (len%x == 0){
+			for (int i = 0; i < len / x; i++){
+				for (int j = 0; j < y; j++){
+					a[i][j] = p[i*y + j];
+					ii++;
+					if (ii == len ){
+						return 0;
+					}
+				}
+				//cout << "执行了shang" << endl;
+			}
+		}
+		else{
+			for (int i = 0; i < (len / x)+1; i++){
+				for (int j = 0; j < y; j++){
+					a[i][j] = p[i*y + j];
+					ii++;
+					if (ii == len ){
+						return 0;
+					}
+				}
+				//cout << "执行了xia" << endl;
+			}
+		}
+		return 0;
+	}
+	void show(){
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				cout << a[i][j] << "\t";
+			}
+			cout << endl;
+		}
+	}
+};
+
+
+
+//void main()
+//{
+//	int rs = 7 / 4;
+//	//cout << rs;
+//	int x, y;
+//	cin >> x >> y;
+//	shuzhu s(x,y);
+//	
+//	
+//
+//	int p[] = {1,2,3,4,5,6,7,8};
+//	int len = sizeof(p) / sizeof(p[0]);
+//	//cout << "  len1：" << len << endl;
+//	s.set(p);
+//	s.show();
+//	
+//}
+
+
+#endif
---
+++ 图论/图论/邻接矩阵到可达矩阵.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include"矩阵.cpp"
+using namespace std;
+
+
+//关联矩阵转化为邻接矩阵（有向图）
+
+//扫描每一行中  如果为正 取这一列中正数的行数为行数  取那一列中负数的行数为列数  加一
+class Tools{
+public :
+
+	static shuzhu lintoke(shuzhu linjie){
+		int x = linjie.x;
+		shuzhu R = juzhen::R(x);
+		shuzhu A0 = juzhen::add(R, linjie);
+		while (juzhen::comp(juzhen::chen(A0, A0), A0)){
+			A0 = juzhen::chen(A0, A0);
+		}
+		return A0;
+	}
+	static shuzhu guantolin(shuzhu guanlian){
+		int a, b;
+		shuzhu linjie(guanlian.x, guanlian.x);
+		for (int i = 0; i < guanlian.y; i++){
+			for (int j = 0; j < guanlian.x; j++){
+				if (guanlian.a[j][i] == 1){
+					a = j;;
+				}
+				if (guanlian.a[j][i] == -1){
+					b = j;
+				}
+
+			}
+			linjie.a[a][b]++;
+		}
+		return linjie;
+	}
+};
+
+void main(){
+	
+	int s[]= { -1, 1, 0, 0, 0, 1, -1, 1, 0, 0,0, 0, 0, 1, 1, 0, 0, -1, -1, -1};
+	shuzhu guanlian(4, 5);
+	guanlian.set(s);
+	shuzhu linjie=Tools::guantolin(guanlian);
+	//linjie.show();
+	shuzhu keda = Tools::lintoke(linjie);
+	keda.show();
+
+
+
+
+}
